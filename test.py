8df9e1e550259f6ba051a77776a0b16c15a94343
Nguyen Minh Duc <s4125574@rmit.edu.vn>
2026-08-10 11:38:53 +0700

    Function 2

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-""" ---Function1--- """
+"""----- Function 1 -----"""
 # complexity = O(n)
 def longest_rising_sublist(readings):
     if not readings:
@@ -14,8 +14,38 @@ def longest_rising_sublist(readings):
             count = 1
     return max_len
 
+"""----- Function 2 -----"""
+# complexity = O(n^2)
+def process_text_editor(actions):
+    output = ""
+    for action in actions:
+        if action[0] == "type":
+            output += action[1]
+        if action[0] == "undo":
+            output = output[: len(output) - 1]
+    return output
+
+
+"""---- Client Code ----"""
 if __name__ == '__main__':
-    """ ---Function1--- """
-    # numbers = [3, 4, 6, 2, 5, 7, 8, 1]
-    numbers = [5, 5, 5]
-    print(longest_rising_sublist(numbers))
+    """--- Function 1 ---"""
+    # numbers = [3, 4, 6, 2, 5, 7, 8, 1]   # Expected output: 4
+    # numbers = [5, 5, 5]   # Expected output: 1
+    # print(longest_rising_sublist(numbers))
+
+    """--- Function 2 ---"""
+    actions = [
+        ("type", "A"),
+        ("type", "B"),
+        ("undo",),
+        ("type", "C")
+    ]   # Expected output: AC
+    # actions = [
+    #     ("type", "A"),
+    #     ("type", "B"),
+    #     ("type", "C"),
+    #     ("undo",),
+    #     ("undo",),
+    #     ("type", "D")
+    # ]   # Expected output: AD
+    print(process_text_editor(actions))
